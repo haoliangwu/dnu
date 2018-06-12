c582db308b446c57a5e5cd3d2c307316c44716a8
Lyon <wuhaoliang@cloudist.cc>
2018-06-12 15:07:01 +0800

    [ADD] add parcel as bundler

--- examples/index.ts
+++ examples/index.ts
@@ -1,5 +1,7 @@
 import DnuClient from '../lib/client'
 const $fileInput: HTMLInputElement = document.querySelector('#fileInput')
+const $serialBtn: HTMLInputElement = document.querySelector('#serial')
+const $parellelBtn: HTMLInputElement = document.querySelector('#parellel')
 
 const client = new DnuClient({
   chunkSize: 1024 * 1024 * 1,
@@ -8,14 +10,33 @@ const client = new DnuClient({
   host: 'http://127.0.0.1:3000'
 })
 
+let uploadMeta = {
+  name: null,
+  data: null
+}
+
+function guard (cb: any) {
+  if (!uploadMeta.data) return alert('select big file first')
+  else cb && cb()
+}
+
 $fileInput.addEventListener('change', () => {
   const file = $fileInput.files[0]
 
   const fileReader = new FileReader()
 
   fileReader.addEventListener('load', () => {
-    client.upload(file.name, fileReader.result)
+    uploadMeta.name = file.name
+    uploadMeta.data = fileReader.result
   })
 
   fileReader.readAsArrayBuffer(file)
+})
+
+$serialBtn.addEventListener('click', () => {
+  guard(() => client.upload(uploadMeta.name, uploadMeta.data))
+})
+
+$parellelBtn.addEventListener('click', () => {
+  guard(() => client.upload(uploadMeta.name, uploadMeta.data, { concurrency: 2 }))
 })
